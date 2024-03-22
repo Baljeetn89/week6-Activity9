1b21ee1423ff68ccfe82bdfac1732d63e136a3c6
Baljeet Kaur <Baljeet.NA@metropolia.fi>
2024-03-22 13:47:15 +0200

    week6-Activity9

---
+++ model.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const orderSchema = new mongoose.Schema({
+    customer: { type: String, required: true },
+    totalAmount: { type: Date, required: true },
+    duration: { type: Number, required: true },
+    products: { type: String },
+    status: { type: String }
+  });
+module.exports = mongoose.model('Order', orderSchema);
